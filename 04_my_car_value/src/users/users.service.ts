9f40cae17bcd1b4c9fcffa99d95b109ea116d07e
Sumit Paul <pauls1317@gmail.com>
2024-12-18 20:42:31 +0530

    create vs save - entity hooks

--- 04_my_car_value/src/users/users.service.ts
+++ 04_my_car_value/src/users/users.service.ts
@@ -8,8 +8,15 @@ export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
   create(email: string, password: string) {
+    // create instance of an entity
+    // NOTE: Important to create entity instance in order to trigger hooks tied to entity
     const user = this.repo.create({ email, password });
 
+    // write to the db
+    // saving direct object - this won't trigger the entity hooks
+    // this.repo.save({ email: 'abc@xyz.com', password: '1234' })
+
+    // saving entity instance - this will trigger the hooks
     return this.repo.save(user);
   }
 }
