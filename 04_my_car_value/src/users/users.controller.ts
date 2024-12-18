5ab326e8b76cd7be76c2066b71468d1c4ec8c669
Sumit Paul <pauls1317@gmail.com>
2024-12-18 21:22:58 +0530

    remove user controller

--- 04_my_car_value/src/users/users.controller.ts
+++ 04_my_car_value/src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -20,5 +28,10 @@ export class UsersController {
   @Get()
   findAllUsers(@Query('email') email: string) {
     return this.userService.find(email);
+  }
+
+  @Delete('/:id')
+  deleteUser(@Param('id') id: string) {
+    return this.userService.remove(parseInt(id));
   }
 }
