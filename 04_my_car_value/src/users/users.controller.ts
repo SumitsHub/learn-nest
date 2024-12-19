ed4878b32ed08b816f7426a6e50471218fc2eb6e
Sumit Paul <pauls1317@gmail.com>
2024-12-19 16:07:44 +0530

    custom wrapper decorator

--- 04_my_car_value/src/users/users.controller.ts
+++ 04_my_car_value/src/users/users.controller.ts
@@ -14,7 +14,10 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
-import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+import {
+  Serialize,
+  SerializeInterceptor,
+} from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
@@ -27,7 +30,8 @@ export class UsersController {
     this.userService.create(body.email, body.password);
   }
 
-  @UseInterceptors(new SerializeInterceptor(UserDto))
+  // @UseInterceptors(new SerializeInterceptor(UserDto))
+  @Serialize(UserDto)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     console.log('Handler is running');
