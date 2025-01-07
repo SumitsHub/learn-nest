71e7f94b0eafcdba58848ae1df13843cd63994b2
Sumit Paul <pauls1317@gmail.com>
2025-01-07 17:06:27 +0530

    followup test - whoami - cookie

--- 04_my_car_value/test/auth.e2e-spec.ts
+++ 04_my_car_value/test/auth.e2e-spec.ts
@@ -32,4 +32,25 @@ describe('Authentication System', () => {
         expect(response.body.email).toEqual(email);
       });
   });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'qwert@yui.op';
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({
+        email,
+        password: 'password',
+      })
+      .expect(201);
+
+    // 'supertest' agent does not handle cookie well, so we need to extract it manually
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie) // attach the cookie to the request
+      .expect(200);
+
+    expect(body.email).toEqual(email);
+  });
 });
