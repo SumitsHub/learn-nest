1a3755e4ea97b9157501c937dbff05937c9ee7b0
Sumit Paul <pauls1317@gmail.com>
2025-01-08 09:31:49 +0530

    create report dto validations

--- 04_my_car_value/src/reports/dtos/create-report.dto.ts
+++ 04_my_car_value/src/reports/dtos/create-report.dto.ts
@@ -1,15 +1,37 @@
+import {
+  IsLatitude,
+  IsLongitude,
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
 export class CreateReportDto {
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
   price: number;
 
+  @IsNumber()
+  @Min(1950)
+  @Max(new Date().getFullYear())
   year: number;
 
+  @IsString()
   make: string;
 
+  @IsString()
   model: string;
 
+  @IsLongitude()
   lng: number;
 
+  @IsLatitude()
   lat: number;
 
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
   mileage: number;
 }
