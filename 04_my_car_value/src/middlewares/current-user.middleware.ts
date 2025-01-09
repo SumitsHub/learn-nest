4d57646767054ca26ded8f2d148a3b8567fac12e
Sumit Paul <pauls1317@gmail.com>
2025-01-09 09:25:49 +0530

    adding an optional field to Express Request

--- 04_my_car_value/src/middlewares/current-user.middleware.ts
+++ 04_my_car_value/src/middlewares/current-user.middleware.ts
@@ -1,6 +1,20 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 import { UsersService } from '../users/users.service';
+import { User } from '../users/user.entity';
+
+// Extending the Request interface to include currentUser - this is a global declaration for Express Request object in TypeScript (not specific to NestJS)
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: User;
+    }
+  }
+}
+
+// declare - used to define a new type in TypeScript (global declaration)
+// namespace - used to group related types
+//* NOTE: reopening an interface extends it with new properties
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
@@ -14,7 +28,6 @@ export class CurrentUserMiddleware implements NestMiddleware {
 
     if (userId) {
       const user = await this.userService.findOne(userId);
-      // @ts-ignore
       req.currentUser = user;
     }
 
