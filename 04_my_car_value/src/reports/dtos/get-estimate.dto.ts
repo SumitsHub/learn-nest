55da703067b62ac034b66f6e74e1512348f8e6e7
Sumit Paul <pauls1317@gmail.com>
2025-01-09 09:37:00 +0530

    get estimate DTO

---
+++ 04_my_car_value/src/reports/dtos/get-estimate.dto.ts
@@ -0,0 +1,32 @@
+import {
+  IsLatitude,
+  IsLongitude,
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
+export class GetEstimateDto {
+  @IsNumber()
+  @Min(1950)
+  @Max(new Date().getFullYear())
+  year: number;
+
+  @IsString()
+  make: string;
+
+  @IsString()
+  model: string;
+
+  @IsLongitude()
+  lng: number;
+
+  @IsLatitude()
+  lat: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  mileage: number;
+}
