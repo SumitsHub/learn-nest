50abe88c370b92190263d09166819b92e68d1f09
Sumit Paul <pauls1317@gmail.com>
2025-01-09 17:30:45 +0530

    query to calculate estimate using createQueryBuilder

--- 04_my_car_value/src/reports/reports.service.ts
+++ 04_my_car_value/src/reports/reports.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { Report } from './report.entity';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { User } from '../users/user.entity';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Injectable()
 export class ReportsService {
@@ -29,5 +30,21 @@ export class ReportsService {
     report.approved = approved;
 
     return this.repo.save(report);
+  }
+
+  createEstimate({ make, model, lng, lat, year, mileage }: GetEstimateDto) {
+    return this.repo
+      .createQueryBuilder() // this is how we create a custom query with TypeORM
+      .select('AVG(price)', 'price') // select the average price
+      .where('make = :make', { make }) // filter by make
+      .andWhere('model = :model', { model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng }) // difference between the longitude of the car and the one we are comparing with should be between -5 and 5
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat }) // difference between the latitude of the car and the one we are comparing with should be between -5 and 5
+      .andWhere('year - :year BETWEEN -3 AND 3', { year }) // difference between the year of the car and the one we are comparing with should be between -3 and 3
+      .andWhere('approved IS TRUE') // only approved reports
+      .orderBy('ABS(mileage - :mileage)', 'DESC') // order by the difference between the mileage of the car and the one we are comparing with
+      .setParameters({ mileage }) // set the mileage parameter for the query builder
+      .limit(3) // limit the results to 3
+      .getRawOne(); // get the result as a raw object
   }
 }
